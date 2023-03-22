275fd43825cadfc9b26de48ace4da45092ff075c
Manpreet <mp_manpreet@hotmail.com>
2023-03-22 05:10:56 +0000

    initial commit

---
+++ iss.js
@@ -0,0 +1,19 @@
+const request = require('request');
+
+const fetchMyIP = function(callback) {
+  // use request to fetch IP address from JSON API
+  request('https://api.ipify.org?format=json', (error, response, body) => {
+    // handle errors
+    if (error) return callback(error, null);
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      return callback(Error(msg), null);
+    }
+    // parse IP address from JSON response
+    const ip = JSON.parse(body).ip;
+    //callback(null, ip);
+    callback(null, ip);
+  });
+};
+
+module.exports = { fetchMyIP };
